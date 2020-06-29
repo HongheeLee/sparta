23835ea9d7172bb65b7de2f3a686f19c6ba8d379
HongheeLee <floppyhong@gmail.com>
2020-06-29 11:33:26 +0900

    [ADD] 검색 결과 나오기 전 기존 텍스트 추가
    검색 결과 나오기 전 '검색 결과 불러오는 중입니다' 텍스트 나와있다가 결과 나오면 바뀌는 화면으로 구현

--- myproject/YES_LIBRARY/app.py
+++ myproject/YES_LIBRARY/app.py
@@ -46,7 +46,6 @@ def sogang_search(keyword):
 
     # 크롤링 토대
     req = driver.page_source
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     soup = BeautifulSoup(req, 'html.parser')
     lis = soup.select("#catalogs > ul > li")
     sogang_list = []
